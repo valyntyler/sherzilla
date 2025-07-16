cf468428d930a00ffbda3c194f7060e4e4af0dd4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-16 08:53:26 +0300

    feat: update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,21 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    sherlock.url = "github:Skxxtz/sherlock";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    sherlock,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {inherit system;};
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             just
             nushell
+            sherlock.packages.${system}.default
           ];
         };
       }
